e91cbed0fb51ef051587f0ea9aea35e5e4076abe
marija <kuveljic117@gmail.com>
2021-04-18 16:37:20 +0200

    Minor fix in registration cosntroller.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super
+    super do
+      resource
+    end
   end
 
   # GET /resource/edit
@@ -42,7 +44,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+
   end
 
   # If you have extra params to permit, append them to the sanitizer.
